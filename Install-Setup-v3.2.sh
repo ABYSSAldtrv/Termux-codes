1830c53b7001b4e2392d6f35f8208537e6f82802
ABYSSAldtrv <162459390+ABYSSAldtrv@users.noreply.github.com>
2025-03-08 18:08:28 -0300

    Atualizar o Install-Setup-v3.3.sh

--- Install-Setup-v3.2.sh
+++ Install-Setup-v3.2.sh
@@ -9,6 +9,14 @@ termux-change-repo
 # Instalação dos pacotes corrigidos
 pkg install -y clang lua53 python-pip nodejs ruby
 
+# Instala C++ (Compilador e ferramentas)
+echo "Instalando C++..."
+pkg install -y clang make cmake g++
+
+# Instala C# (Mono)
+echo "Instalando C#..."
+pkg install -y mono
+
 # Criar link simbólico para usar 'lua' em vez de 'lua53'
 ln -sf /data/data/com.termux/files/usr/bin/lua53 /data/data/com.termux/files/usr/bin/lua
 
